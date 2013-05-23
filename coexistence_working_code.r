7bc42cec79a20c03750cee893591e0a2b28d28d0
Xiao Xiao <xiao@weecology.org>
2013-05-23 17:26:08 -0600

    Add new analyses and plots
    - Add analyses for the effect of a1 and a2 on coexistence.
    - Add code to create Fig. 4 and Fig. 5.

--- coexistence_working_code.r
+++ coexistence_working_code.r
@@ -19,6 +19,13 @@ num_range = function(b1, b2){
   coexist_range(coexist_AM, 1/(b1+1)/100, 1/(b1+1), 1/(b2+1)/100, 1/(b2+1), par_without_d, out_file, t = 10000)
 }
 
+num_range_a_b = function(a1, a2, b1, b2){
+# Modified num_range() which allows a1 and a2 to vary as well as b1 and b2.
+  par_without_d = c(a1 = a1, a2 = a2, b1 = b1, b2 = b2)
+  out_file = paste('NumRange_', a1, '_', a2, '_', b1, '_', b2, '.csv', sep = "")
+  coexist_range(coexist_AM, a1/(b1+1)/100, a1/(b1+1), a2/(b2+1)/100, a2/(b2+1), par_without_d, out_file, t = 10000)
+}
+
 growth_rate = function(a, b, d, x){
 # Returns the value of the functional response at x, given functional response
 #   parameters a and b, and mortality d
@@ -71,6 +78,16 @@ for (b2 in seq(0, 9.5, 0.5)){
   }
 }
 
+# Create a data frame with all combinations of (a1, a2),
+#   with a1 and a2 varying independently from 1 to 5 in intervals of 0.5
+a_list = as.data.frame(matrix(nrow = 0, ncol = 2))
+names(a_list) = c('a1', 'a2')
+for (a1 in seq(1, 10, 0.5)){
+  for (a2 in seq(1, 10, 0.5)){
+    a_list = rbind(a_list, c(a1, a2))
+  }
+}
+
 # Evaluate region of coexistence for all (b1, b2) combinations
 # This analysis has been caried out in cluster with 8 cores,
 #   as it is very time-consuming.
@@ -79,6 +96,16 @@ foreach(i=1:dim(b_list)[1]) %dopar% {
   library(deSolve)
   source('coexistence_module.r')
   num_range(b_list[i, 1], b_list[i, 2])
+}
+
+# Evaluate region of coexistence for all (b1, b2) combinations
+#   with b1 fixed at 10 and b2 fixed at 0 or 3
+registerDoSNOW(makeCluster(8, type = "SOCK"))
+foreach(i=1:dim(a_list)[1]) %dopar% {
+  library(deSolve)
+  source('coexistence_module.r')
+  num_range_a_b(a_list[i, 1], a_list[i, 2], 10, 0)
+  num_range_a_b(a_list[i, 1], a_list[i, 2], 10, 3)
 }
 
 ##################################
@@ -161,12 +188,12 @@ title_list = c('(a) Area of Potential Region of Coexistence',
 plot1 = wireframe(b_grid[, 3] ~ b1 * b2, b_grid, scales = list(arrows = F, distance = 1.1),
   screen = list(z = -40, x = -70), xlab = list(expression(b[1]), cex = 1.5),
   ylab = list(expression(b[2]), cex = 1.5), zlab = list(expression(A[potential]), cex = 1.5, rot = 90),
-  main ='(a) Area of Potential Region of Coexistence', col.regions = '#EDEDED')
+  zlim = c(0, max(b_grid[, 3])), main ='(a) Area of Potential Region of Coexistence', col.regions = '#EDEDED')
 
 plot2 = wireframe(b_grid[, 4] ~ b1 * b2, b_grid, scales = list(arrows = F, distance = 1.1),
   screen = list(z = -40, x = -70), xlab = list(expression(b[1]), cex = 1.5),
   ylab = list(expression(b[2]), cex = 1.5), zlab = list(expression(A[realized]), cex = 1.5, rot = 90),
-  main ='(b) Area of Realized Region of Coexistence', col.regions = '#EDEDED')
+  zlim = c(0, max(b_grid[, 3])), main ='(b) Area of Realized Region of Coexistence', col.regions = '#EDEDED')
 
 plot3 = wireframe(b_grid[, 5] ~ b1 * b2, b_grid, scales = list(arrows = F, distance = 1.1),
   screen = list(z = -40, x = -70), xlab = list(expression(b[1]), cex = 1.5),
@@ -200,5 +227,117 @@ for (i in 1:length(b1_list)){
   b2 = b2_list[i]
   plot_pot_rea(b1, b2, x_lim, y_lim, main_lab_list[i + 3])
 }
+
+dev.off()
+
+#### Figure 4 ####
+png(filename = 'Fig4.png', width = 800, height = 800)
+
+a_grid = expand.grid(a1 = do.breaks(c(1, 5), 8), a2 = do.breaks(c(1, 5), 8))
+for (i in 1:dim(a_grid)[1]){
+    a1 = a_grid[i, 1]
+    a2 = a_grid[i, 2]
+    A_ratio = 0
+    A_pot = compute_A_pot(a1, a2, 10, 0)
+    dat = read.csv(paste('NumRange_', a1, '_', a2, '_10_0.csv', sep = ''),
+      colClasses = rep('numeric', 2))
+    num_points = dim(dat)[1]
+    A_tot = a1/(10+1) * a2/(0+1) # Area of the rectangular region (A+B+C+D)
+    A_prop = num_points / 10000
+    A_rea = A_prop * A_tot
+    if (num_points >= 50){
+      A_ratio = A_rea / A_pot
+    }
+    a_grid$z_pot[i] = A_pot
+    a_grid$z_rea[i] = A_rea
+    a_grid$z_ratio[i] = A_ratio
+    a_grid$z_prop[i] = A_prop
+}
+
+z_lab_list = c('A', 'A', 'Ratio', 'Proportion')
+title_list = c('(a) Area of Potential Region of Coexistence',
+  '(b) Area of Realized Region of Coexistence',
+  '(c) Ratio between Potential and Realized Regions',
+  '(d) Proportion of Realized Region\n in Full Rectangular Region'
+)
+
+plot1 = wireframe(a_grid[, 3] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5), zlab = list(expression(A[potential]), cex = 1.5, rot = 90),
+  zlim = c(0, max(a_grid[, 3])), main ='(a) Area of Potential Region of Coexistence', col.regions = '#EDEDED')
+
+plot2 = wireframe(a_grid[, 4] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5), zlab = list(expression(A[realized]), cex = 1.5, rot = 90),
+  zlim = c(0, max(a_grid[, 3])), main ='(b) Area of Realized Region of Coexistence', col.regions = '#EDEDED')
+
+plot3 = wireframe(a_grid[, 5] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5), zlab = list('Ratio', cex = 1.5, rot = 90),
+  zlim = c(max(a_grid[, 5])/2, max(a_grid[, 5])), main ='(c) Ratio between Potential and Realized Regions', col.regions = '#EDEDED')
+
+plot4 = wireframe(a_grid[, 6] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5),
+  zlab = list('Proportion', cex = 1.5, rot = 90),
+  zlim = c(max(a_grid[, 6])/2, max(a_grid[, 6])), main = '(d) Proportion of Realized Region in Rectangular Region', col.regions = '#EDEDED')
+
+grid.arrange(plot1, plot2, plot3, plot4, nrow = 2, ncol = 2)
+
+dev.off()
+
+#### Figure 5 ####
+png(filename = 'Fig5.png', width = 800, height = 800)
+
+a_grid = expand.grid(a1 = do.breaks(c(1, 5), 8), a2 = do.breaks(c(1, 5), 8))
+for (i in 1:dim(a_grid)[1]){
+    a1 = a_grid[i, 1]
+    a2 = a_grid[i, 2]
+    A_ratio = 0 
+    A_pot = compute_A_pot(a1, a2, 10, 3)
+    dat = read.csv(paste('NumRange_', a1, '_', a2, '_10_3.csv', sep = ''),
+      colClasses = rep('numeric', 2))
+    num_points = dim(dat)[1]
+    A_tot = a1/(10+1) * a2/(3+1) # Area of the rectangular region (A+B+C+D)
+    A_prop = num_points / 10000
+    A_rea = A_prop * A_tot
+    if (num_points >= 50){
+      A_ratio = A_rea / A_pot
+    }
+    a_grid$z_pot[i] = A_pot
+    a_grid$z_rea[i] = A_rea
+    a_grid$z_ratio[i] = A_ratio
+    a_grid$z_prop[i] = A_prop
+}
+
+z_lab_list = c('A', 'A', 'Ratio', 'Proportion')
+title_list = c('(a) Area of Potential Region of Coexistence',
+  '(b) Area of Realized Region of Coexistence',
+  '(c) Ratio between Potential and Realized Regions',
+  '(d) Proportion of Realized Region\n in Full Rectangular Region'
+)
+
+plot1 = wireframe(a_grid[, 3] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5), zlab = list(expression(A[potential]), cex = 1.5, rot = 90),
+  zlim = c(0, max(a_grid[, 3])), main ='(a) Area of Potential Region of Coexistence', col.regions = '#EDEDED')
+
+plot2 = wireframe(a_grid[, 4] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5), zlab = list(expression(A[realized]), cex = 1.5, rot = 90),
+  zlim = c(0, max(a_grid[, 3])), main ='(b) Area of Realized Region of Coexistence', col.regions = '#EDEDED')
+
+plot3 = wireframe(a_grid[, 5] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5), zlab = list('Ratio', cex = 1.5, rot = 90),
+  zlim = c(max(a_grid[, 5])/2, max(a_grid[, 5])), main ='(c) Ratio between Potential and Realized Regions', col.regions = '#EDEDED')
+
+plot4 = wireframe(a_grid[, 6] ~ a1 * a2, a_grid, scales = list(arrows = F, distance = 1.1),
+  screen = list(z = -40, x = -70), xlab = list(expression(a[1]), cex = 1.5),
+  ylab = list(expression(a[2]), cex = 1.5),
+  zlab = list('Proportion', cex = 1.5, rot = 90),
+  zlim = c(max(a_grid[, 6])/2, max(a_grid[, 6])), main = '(d) Proportion of Realized Region in Rectangular Region', col.regions = '#EDEDED')
+
+grid.arrange(plot1, plot2, plot3, plot4, nrow = 2, ncol = 2)
 
 dev.off()
